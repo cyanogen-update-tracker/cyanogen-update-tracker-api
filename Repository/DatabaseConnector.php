f31ad44ba67295f7337f35488048a9322612d34a
Arjan Vlek <arjan1995@gmail.com>
2015-11-22 10:58:49 +0100

    Updated db config Added Wileyfox storm data Added BQ Aquaris X5 data Updated zuk z1 data

--- Repository/DatabaseConnector.php
+++ Repository/DatabaseConnector.php
@@ -3,7 +3,7 @@
 class databaseConnector {
     public function connectToDb() {
         $username = "** Add your database username here **";
-        $password = "Stoomtrein1";
+        $password = "** Add your database password here **";
         $server_address = "localhost";
         $database_name = "cyanogen_update_tracker";
         $database = new PDO('mysql:host='.$server_address.';dbname='.$database_name.'',$username, $password);
