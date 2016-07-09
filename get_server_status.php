48e55a3a7ca8e18dceb0e1729426894413c815ed
Arjan Vlek <arjan.vlek@42.nl>
2016-07-09 13:38:59 +0200

    Fixed serverStatus bug

--- get_server_status.php
+++ get_server_status.php
@@ -10,7 +10,7 @@ $query = $database->query("SELECT status, latest_app_version FROM server_status"
 
 // Return the output as JSON
 header('Content-type: application/json');
-echo (json_encode($query->fetchAll(PDO::FETCH_ASSOC)));
+echo (json_encode($query->fetch(PDO::FETCH_ASSOC)));
 
 // Disconnect from the database
 $database = null;
