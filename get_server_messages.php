6a1863e81e4240ee031ee751d2744da3a6ef23df
Arjan Vlek <arjan.vlek@42.nl>
2016-07-09 13:47:48 +0200

    Server messages are now filtered on device and update method at the server side.

--- get_server_messages.php
+++ get_server_messages.php
@@ -5,9 +5,16 @@ include "Repository/DatabaseConnector.php";
 $databaseConnector = new DatabaseConnector();
 $database = $databaseConnector->connectToDb();
 
-// Execute the query
-$query = $database->query("SELECT id, message, message_nl, device_id, update_method_id, priority, marquee FROM server_message WHERE enabled = TRUE");
+// Obtain all required request parameters.
+$deviceId = $_GET["device_id"];
+$updateMethodId = $_GET["update_method_id"];
 
+
+// Execute the query
+$query = $database->prepare("SELECT id, message, message_nl, device_id, update_method_id, priority, marquee FROM server_message WHERE (device_id IS NULL OR device_id = :device_id) AND (update_method_id IS NULL OR server_message.update_method_id = :update_method_id) AND enabled = TRUE");
+$query->bindParam(":device_id", $deviceId);
+$query->bindParam(":update_method_id", $updateMethodId);
+$query->execute();
 // Return the output as JSON
 header('Content-type: application/json');
 echo (json_encode($query->fetchAll(PDO::FETCH_ASSOC)));
